f155704dfebe0b955ee74456a5ec200841769055
Nikhil Shindikar <shindikarnikhil@gmail.com>
2025-04-23 19:18:10 +0530

    Edit pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -10,19 +10,15 @@ from rasterio.transform import rowcol, xy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import threading
-from dronekit import connect, VehicleMode, Command
-from pymavlink import mavutil
-
-# ========== CONFIGURATION ==========
+#------------------------------------------------------------------------------------------------------#
+# Configuration Variables
 TIFF_PATH = "Mumbai 7500.tif"
 REFERENCE_SIZE = 500
 INITIAL_GPS = (18.992394752836383, 72.83529905339414)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPV4, UDP
 out_addr = ("127.0.0.1", 25100)
-
-# Connect to SITL
-# vehicle = connect('127.0.0.1:14550', wait_ready=True)
-# ========== CAMERA CLASS ==========
+#------------------------------------------------------------------------------------------------------#
+# Function to get distance between two GPS coordinates 
 def haversine(coord1, coord2):
     """Calculate the Haversine distance between two (lat, lon) points."""
     R = 6371000  # Earth radius in meters
@@ -38,18 +34,47 @@ def haversine(coord1, coord2):
 
     return R * c
 
+# Function to send GPS data over UDP
+def send_gps_udp(lat, lon, angle):
 
+    gps_msg = {
+        'time_usec': 0,
+        'gps_id': 0,
+        'ignore_flags': 254,  # ignore alt, hdop, vdop, vn, ve, vd, speed_accuracy, vert_accuracy
+        'time_week_ms': 0,
+        'time_week': 0,
+        'fix_type': 3,
+        'lat': int(lat * 1E7),
+        'lon': int(lon * 1E7),
+        'alt': 0,
+        'hdop': 1,
+        'vdop': 1,
+        'vn': 0,
+        've': 0,
+        'vd': 0,
+        'speed_accuracy': 0,
+        'horiz_accuracy': 1,
+        'vert_accuracy': 0,
+        'satellites_visible': 13,
+        'yaw': angle  
+    }
+    out_data = json.dumps(gps_msg)
+    s.sendto(out_data.encode(), out_addr)
+
+# Camera class to handle image capture
 class Camera:
     def __init__(self, topic="/webcam/image_raw"):
         self.bridge = CvBridge()
         self.image = None
+
+        # Mutex lock to prevent conflict between Image saving and retrieval
         self.lock = threading.Lock()
         rospy.Subscriber(topic, Image, self.callback)
         rospy.loginfo(f"Subscribed to camera topic: {topic}")
 
     def callback(self, msg):
         try:
-            # Convert to BGR color image
+            # Save grayscale image
             img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="mono8")
             with self.lock:
                 self.image = img.copy()
@@ -59,9 +84,9 @@ class Camera:
     def get_image(self):
         with self.lock:
             return self.image.copy() if self.image is not None else None
-        
 
-# ========== MAIN LOOP ==========
+#------------------------------------------------------------------------------------------------------#
+# Main function to process images and send GPS data
 def main():
     rospy.init_node('gps_image_matcher')
     sift = cv2.SIFT_create()
@@ -69,6 +94,7 @@ def main():
     camera = Camera()
     current_gps = INITIAL_GPS
 
+    # Load GeoTIFF raster image
     with rasterio.open(TIFF_PATH) as dataset:
 
         image = dataset.read()
@@ -83,6 +109,7 @@ def main():
         height_m = haversine((top_left_latlon[1], top_left_latlon[0]), (bottom_left_latlon[1], bottom_left_latlon[0]))
         width_m = haversine((bottom_left_latlon[1], bottom_left_latlon[0]), (bottom_right_latlon[1], bottom_right_latlon[0]))
 
+        # Calculate average meters per pixel accuracy
         h_acc = 0.5 * ((width_m/width) + (height_m/height)) 
 
         count = 0
@@ -90,15 +117,13 @@ def main():
 
             if count < 5:
                 start_time = time.time()
+
                 # Get the current camera image
                 cam_gray = camera.get_image()
                 if cam_gray is None:
                     rospy.logwarn("Waiting for camera image...")
                     rospy.sleep(0.1)
                     continue
-
-                # cv2.imshow("Camera Image", cam_img)
-                # cv2.waitKey(1)
 
                 ref_row, ref_col = rowcol(dataset.transform, current_gps[1], current_gps[0])
 
@@ -108,12 +133,12 @@ def main():
                 col_start = max(0, ref_col - REFERENCE_SIZE // 2)
                 col_end = min(dataset.width, ref_col + REFERENCE_SIZE // 2)
 
-                # Read RGB bands (assuming bands 1, 2, 3 are R, G, B)
+                # Read RGB bands from the raster image
                 r = dataset.read(1, window=((row_start, row_end), (col_start, col_end)))
                 g = dataset.read(2, window=((row_start, row_end), (col_start, col_end)))
                 b = dataset.read(3, window=((row_start, row_end), (col_start, col_end)))
 
-                # Stack and convert to BGR (OpenCV expects BGR)
+                # Stack and convert to BGR 
                 ref_image = np.dstack((b, g, r))
                 ref_center = ((row_start+row_end)//2, (col_start+col_end)//2)
 
@@ -148,7 +173,8 @@ def main():
                     count+=1
                     rospy.sleep(0.1)
                     continue
-
+                
+                # Reset count of failed image matching attempts
                 count=0
                 
                 # Get matched points
@@ -174,72 +200,47 @@ def main():
                 # Calculate center coordinates relative to reference center
                 mean_x = np.mean(ref_corners[:,:,0])
                 mean_y = np.mean(ref_corners[:,:,1])
-                center_x = mean_y - ref_center[1]  # X = y - center_y (down +)
-                center_y = mean_x - ref_center[0]  # Y = x - center_x (right +)
 
+                # Center pixel coordinates of camera image 
+                # wrt Raster map image
                 pixel_row = mean_y + row_start
                 pixel_col = mean_x + col_start
-                            
+
+                # Access GPS coordinates of the center pixel               
                 lon, lat = xy(dataset.transform, pixel_row, pixel_col)
                 current_gps = (lat, lon)
                 print("Latitude:", lat, "Longitude:", lon)
 
                 # Calculate rotation angle using top edge vector
-                tl_img = ref_corners[0][0]  # Top-left in image coordinates (x, y)
-                tr_img = ref_corners[3][0]  # Top-right in image coordinates (x, y)
+                tl_img = ref_corners[0][0]      # Top-left in image coordinates (x, y)
+                tr_img = ref_corners[3][0]      # Top-right in image coordinates (x, y)
                 
                 deltaY = tr_img[0] - tl_img[0]  # Change in image x (Y in user's system)
                 deltaX = tr_img[1] - tl_img[1]  # Change in image y (X in user's system)
                 angle_rad = np.arctan2(deltaX, deltaY)
                 angle_deg = np.degrees(angle_rad)
-                angle_deg = np.mod(angle_deg, 360)  # Normalize to 0-360 degrees
+                angle_deg = 360 - np.mod(angle_deg, 360)  # Yaw angle: Clockwise
 
-        
-                # Send GPS over UDP
-                gps_msg = {
-                        'time_usec' : 0,                        # (uint64_t) Timestamp (micros since boot or Unix epoch)
-                        'gps_id' : 0,                           # (uint8_t) ID of the GPS for multiple GPS inputs
-                        'ignore_flags' : 8,                     # (uint16_t) Flags indicating which fields to ignore (see GPS_INPUT_IGNORE_FLAGS enum). All other fields must be provided.
-                        'time_week_ms' : 0,                     # (uint32_t) GPS time (milliseconds from start of GPS week)
-                        'time_week' : 0,                        # (uint16_t) GPS week number
-                        'fix_type' : 3,                         # (uint8_t) 0-1: no fix, 2: 2D fix, 3: 3D fix. 4: 3D with DGPS. 5: 3D with RTK
-                        'lat' : int(lat * 1E7),                      # (int32_t) Latitude (WGS84), in degrees * 1E7
-                        'lon' : int(lon * 1E7),                      # (int32_t) Longitude (WGS84), in degrees * 1E7
-                        'alt' : 0,                              # (float) Altitude (AMSL, not WGS84), in m (positive for up)
-                        'hdop' : 1,                             # (float) GPS HDOP horizontal dilution of position in m
-                        'vdop' : 1,                             # (float) GPS VDOP vertical dilution of position in m
-                        'vn' : 0,                               # (float) GPS velocity in m/s in NORTH direction in earth-fixed NED frame
-                        've' : 0,                               # (float) GPS velocity in m/s in EAST direction in earth-fixed NED frame
-                        'vd' : 0,                               # (float) GPS velocity in m/s in DOWN direction in earth-fixed NED frame
-                        'speed_accuracy' : 0,                   # (float) GPS speed accuracy in m/s
-                        'horiz_accuracy' : 0,                   # (float) GPS horizontal accuracy in m
-                        'vert_accuracy' : 0,                    # (float) GPS vertical accuracy in m
-                        'satellites_visible' : 13                # (uint8_t) Number of satellites visible.
-                }
-
-                out_data = json.dumps(gps_msg)
-                # print('out:',out_data)
-                s.sendto(out_data.encode(), out_addr)
+                # Feed GPS to SITL
+                # send_gps_udp(lat, lon, angle_deg)
 
                 frequency = 1 / (time.time() - start_time)
                 print("Frequency:", frequency)
 
-                # === Display Reference Image with Matched Center ===
+                # Display Reference Image with Matched Center 
                 # Draw a circle at the matched position
                 marker_x = int(mean_x)
                 marker_y = int(mean_y)
                 cv2.circle(ref_image, (marker_x, marker_y), 10, (0, 0, 255), 2)
 
-                # Resize to 640x480
                 annotated_resized = cv2.resize(ref_image, (480, 480))
-
-                # cv2.imshow("Reference Image Match", annotated_resized)
-                cv2.imshow("Reference Image Match", ref_image)
+                cv2.imshow("Reference Image Match", annotated_resized)
                 cv2.waitKey(1)
 
 
             else:
                 print("Unable to get enough features.")
 
+#------------------------------------------------------------------------------------------------------#
 if __name__ == "__main__":
     main()
